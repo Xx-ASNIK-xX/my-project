f1090b4cb972dd890e703071c6a008b6ee1d0ba1
Xx-ASNIK-xX <nico_distortion@hotmail.com>
2021-12-09 19:58:42 -0300

    feat componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { EjemploPadreComponent } from './components/ejemplo-padre/ejemplo-padre.component';
+import { EjemploHijoComponent } from './components/ejemplo-hijo/ejemplo-hijo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    EjemploPadreComponent,
+    EjemploHijoComponent
   ],
   imports: [
     BrowserModule,
